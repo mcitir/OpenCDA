6d24e932887796bfeb5a4b97223d413a1597cf08
mcitir <35730346+mcitir@users.noreply.github.com>
2023-09-19 22:44:33 +0200

    updating .sh script to enter with different users

--- enter_docker.sh
+++ enter_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Check the provided argument
+# Check the provided argument and run into the docker container
 if [ -z "$1" ] || [ "$1" == "root" ]; then
     docker exec -it opencda_container bash
 elif [ "$1" == "opencda" ]; then
